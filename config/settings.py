dddfe7214991836521f4d8f375ec382247962029
FallenBreath <themercury035@gmail.com>
2026-03-25 10:01:28 +0300

    Fix dotenv loading for deployment

--- config/settings.py
+++ config/settings.py
@@ -2,9 +2,8 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
